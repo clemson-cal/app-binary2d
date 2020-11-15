067685db46945a34fdf11f54151d0de9a0fb538e
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-11-15 17:15:09 -0500

    Port generic version of the CellData struct to scheme_v2

--- src/scheme_v2.rs
+++ src/scheme_v2.rs
@@ -40,10 +40,42 @@ pub struct State<Conserved>
 
 
 // ============================================================================
+#[derive(Copy, Clone)]
+struct CellData<'a, Primitive>
+{
+    pc: &'a Primitive,
+    gx: &'a Primitive,
+    gy: &'a Primitive,
+}
+
+impl<'a, Primitive> CellData<'_, Primitive>
+{
+    fn new(pc: &'a Primitive, gx: &'a Primitive, gy: &'a Primitive) -> CellData<'a, Primitive>
+    {
+        CellData{
+            pc: pc,
+            gx: gx,
+            gy: gy,
+        }
+    }
+}
+
+
+
+
+// ============================================================================
 trait Hydrodynamics
 {
     type Conserved;
     type Primitive;
+    type Direction;
+
+    fn velocity_x        (&self, primitie: &Self::Primitive) -> f64;
+    fn velocity_y        (&self, primitie: &Self::Primitive) -> f64;
+    fn density           (&self, primitie: &Self::Primitive) -> f64;
+    fn gradient_field<'a>(&self, cell_data: &CellData<'a, Self::Primitive>, axis: Self::Direction) -> &'a Self::Primitive;
+    fn strain_field  <'a>(&self, cell_data: &CellData<'a, Self::Primitive>, row: Self::Direction, col: Self::Direction) -> f64;
+    fn stress_field  <'a>(&self, cell_data: &CellData<'a, Self::Primitive>, kinematic_viscosity: f64, row: Self::Direction, col: Self::Direction) -> f64;
 
     fn source_terms(
         &self,
@@ -59,15 +91,49 @@ trait Hydrodynamics
 
 
 
-struct TwoDimensionalIsothermalHydrodynamics;
-
-
-
+// ============================================================================
+struct TwoDimensionalIsothermalHydrodynamics
+{
+}
 
 impl Hydrodynamics for TwoDimensionalIsothermalHydrodynamics
 {
+    type Direction = hydro_iso2d::Direction;
     type Conserved = hydro_iso2d::Conserved;
     type Primitive = hydro_iso2d::Primitive;
+
+    fn velocity_x(&self, primitive: &Self::Primitive) -> f64 { primitive.velocity_x() }
+    fn velocity_y(&self, primitive: &Self::Primitive) -> f64 { primitive.velocity_y() }
+    fn density   (&self, primitive: &Self::Primitive) -> f64 { primitive.density() }
+
+    fn gradient_field<'a>(&self, cell_data: &CellData<'a, Self::Primitive>, axis: Self::Direction) -> &'a Self::Primitive
+    {
+        use hydro_iso2d::Direction::{X, Y};
+
+        match axis
+        {
+            X => cell_data.gx,
+            Y => cell_data.gy,
+        }
+    }
+
+    fn strain_field<'a>(&self, cell_data: &CellData<'a, Self::Primitive>, row: Self::Direction, col: Self::Direction) -> f64
+    {
+        use hydro_iso2d::Direction::{X, Y};
+
+        match (row, col)
+        {
+            (X, X) => cell_data.gx.velocity_x() - cell_data.gy.velocity_y(),
+            (X, Y) => cell_data.gx.velocity_y() + cell_data.gy.velocity_x(),
+            (Y, X) => cell_data.gx.velocity_y() + cell_data.gy.velocity_x(),
+            (Y, Y) =>-cell_data.gx.velocity_x() + cell_data.gy.velocity_y(),
+        }
+    }
+
+    fn stress_field<'a>(&self, cell_data: &CellData<'a, Self::Primitive>, kinematic_viscosity: f64, row: Self::Direction, col: Self::Direction) -> f64
+    {
+        kinematic_viscosity * cell_data.pc.density() * self.strain_field(cell_data, row, col)
+    }
 
     fn source_terms(
         &self,
