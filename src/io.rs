41e46cb839819785153bfeab1c1266f1692ca929
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-11-25 07:40:28 -0500

    Sketch out ideas for H5Write / H5Read traits

--- src/io.rs
+++ src/io.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
-use hdf5::{File, H5Type};
+use hdf5::{File, Group, H5Type};
 use io_logical::verified;
 
 
 
 
 // ============================================================================
-fn write_group<E: H5Type, T: Into<Vec<(String, E)>>>(group: &hdf5::Group, tasks: T) -> Result<(), hdf5::Error>
+fn write_group<E: H5Type, T: Into<Vec<(String, E)>>>(group: &Group, tasks: T) -> Result<(), hdf5::Error>
 {
     let task_vec: Vec<_> = tasks.into();
     for (name, task) in task_vec {
@@ -15,7 +15,7 @@ fn write_group<E: H5Type, T: Into<Vec<(String, E)>>>(group: &hdf5::Group, tasks:
     Ok(())
 }
 
-fn read_group<E: H5Type, T: From<Vec<(String, E)>>>(group: &hdf5::Group) -> Result<T, hdf5::Error>
+fn read_group<E: H5Type, T: From<Vec<(String, E)>>>(group: &Group) -> Result<T, hdf5::Error>
 {
     let task_vec: Vec<_> = group.member_names()?
         .into_iter()
@@ -28,16 +28,96 @@ fn read_group<E: H5Type, T: From<Vec<(String, E)>>>(group: &hdf5::Group) -> Resu
 
 
 
+/**
+ * This trait enables types which are not themselves H5Type, but are Into<T>
+ * where T: H5Type to be read and written transparently.
+ */
+trait IntoH5Type
+{
+    type TargetType: H5Type;
+}
+
+
+
+
+/**
+ * This trait bridges the ndarray and hdf5 Dimension traits.
+ */
+trait Dimension: ndarray::Dimension
+{
+    type Shape: hdf5::Dimension;
+
+    fn hdf5_shape<P: ndarray::RawData>(array: &ndarray::ArrayBase<P, Self>) -> Self::Shape;
+}
+
+impl Dimension for ndarray::Ix1
+{
+    type Shape = usize;
+    fn hdf5_shape<P: ndarray::RawData>(array: &ndarray::ArrayBase<P, Self>) -> Self::Shape { array.dim() }
+}
+
+impl Dimension for ndarray::Ix2
+{
+    type Shape = (usize, usize);
+    fn hdf5_shape<P: ndarray::RawData>(array: &ndarray::ArrayBase<P, Self>) -> Self::Shape { array.dim() }
+}
+
+impl Dimension for ndarray::Ix3
+{
+    type Shape = (usize, usize, usize);
+    fn hdf5_shape<P: ndarray::RawData>(array: &ndarray::ArrayBase<P, Self>) -> Self::Shape { array.dim() }
+}
+
+
+
+
 // ============================================================================
-fn write_state(group: &hdf5::Group, state: &crate::State, block_data: &Vec<crate::BlockData>) -> Result<(), hdf5::Error>
+impl IntoH5Type for hydro_iso2d::Conserved
+{
+    type TargetType = [f64; 3];
+}
+
+
+
+
+// ============================================================================
+fn write_array<P, T, U, I>(group: &Group, name: &str, data: &ndarray::ArrayBase<P, I>) -> Result<(), hdf5::Error>
+    where
+    P: ndarray::RawData<Elem=T> + ndarray::Data,
+    T: Clone + IntoH5Type<TargetType=U>,
+    U: H5Type + From<T>,
+    I: ndarray::Dimension + Dimension,
+{
+    group.new_dataset::<U>().create(name, I::hdf5_shape(data))?.write(&data.mapv(U::from))?;
+    Ok(())
+}
+
+
+
+
+// ============================================================================
+fn read_array<T, U, I: ndarray::Dimension>(group: &Group, name: &str) -> Result<ndarray::Array<T, I>, hdf5::Error>
+    where
+    T: IntoH5Type<TargetType=U>,
+    U: Clone + H5Type + Into<T>
+{
+    Ok(group.dataset(name)?
+        .read_dyn::<U>()?
+        .into_dimensionality::<I>()?
+        .mapv(Into::<T>::into))
+}
+
+
+
+
+// ============================================================================
+fn write_state(group: &Group, state: &crate::State, block_data: &Vec<crate::BlockData>) -> Result<(), hdf5::Error>
 {
     let cons = group.create_group("conserved")?;
 
     for (b, u) in block_data.iter().zip(&state.conserved)
     {
-        let dname = format!("0:{:03}-{:03}", b.index.0, b.index.1);
-        let udata = u.mapv(Into::<[f64; 3]>::into);
-        cons.new_dataset::<[f64; 3]>().create(&dname, u.dim())?.write(&udata)?;
+        write_array(&cons, &format!("0:{:03}-{:03}", b.index.0, b.index.1), &u)?;
     }
 
     group.new_dataset::<i64>().create("iteration", ())?.write_scalar(&state.iteration.to_integer())?;
@@ -53,12 +133,8 @@ pub fn read_state(file: verified::File) -> Result<crate::State, hdf5::Error>
 
     for key in cons.member_names()?
     {
-        let u = cons.dataset(&key)?
-            .read_dyn::<[f64; 3]>()?
-            .into_dimensionality::<ndarray::Ix2>()?
-            .mapv(Into::<hydro_iso2d::Conserved>::into)
-            .to_shared();
-        conserved.push(u);
+        let u = read_array::<hydro_iso2d::Conserved, [f64; 3], ndarray::Ix2>(&cons, &key)?;
+        conserved.push(u.to_shared());
     }
 
     let time      = file.dataset("time")     ?.read_scalar::<f64>()?;
@@ -76,7 +152,7 @@ pub fn read_state(file: verified::File) -> Result<crate::State, hdf5::Error>
 
 
 // ============================================================================
-fn write_tasks(group: &hdf5::Group, tasks: &crate::Tasks) -> Result<(), hdf5::Error>
+fn write_tasks(group: &Group, tasks: &crate::Tasks) -> Result<(), hdf5::Error>
 {
     write_group(group, tasks.clone())
 }
@@ -90,7 +166,7 @@ pub fn read_tasks(file: verified::File) -> Result<crate::Tasks, hdf5::Error>
 
 
 // ============================================================================
-fn write_model(group: &hdf5::Group, model: &HashMap::<String, kind_config::Value>) -> Result<(), hdf5::Error>
+fn write_model(group: &Group, model: &HashMap::<String, kind_config::Value>) -> Result<(), hdf5::Error>
 {
     kind_config::io::write_to_hdf5(&group, &model)
 }
@@ -118,7 +194,12 @@ pub fn read_time_series<T: H5Type>(file: verified::File) -> Result<Vec<T>, hdf5:
 
 
 // ============================================================================
-pub fn write_checkpoint(filename: &str, state: &crate::State, block_data: &Vec<crate::BlockData>, model: &HashMap::<String, kind_config::Value>, tasks: &crate::Tasks) -> Result<(), hdf5::Error>
+pub fn write_checkpoint(
+    filename: &str,
+    state: &crate::State,
+    block_data: &Vec<crate::BlockData>,
+    model: &HashMap::<String, kind_config::Value>,
+    tasks: &crate::Tasks) -> Result<(), hdf5::Error>
 {
     let file = File::create(filename)?;
     let tasks_group = file.create_group("tasks")?;
