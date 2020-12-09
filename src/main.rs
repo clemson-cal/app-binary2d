4c22c651118bb0b8ee60246dbff95ab2ddcc05cc
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-12-09 17:15:33 -0500

    Send model parameters to the IntialModel
    This commit also changes the fiducial disk
    model in isothermal mode to use a finite
    "soft" torus. There are three new model
    parameters: disk_mass, disk_width, and
    disk_radius to control the torus. The total
    disk mass should be << 1 so that it's less
    than the binary.

--- src/main.rs
+++ src/main.rs
@@ -74,6 +74,9 @@ fn main() -> anyhow::Result<()>
         .item("cpi"             , 1.0    , "Checkpoint interval [Orbits]")
         .item("domain_radius"   , 6.0    , "Half-size of the domain [a]")
         .item("hydro"           , "iso"  , "Hydrodynamics mode: [iso|euler]")
+        .item("disk_radius"     , 3.0    , "Disk truncation radius (model-dependent)")
+        .item("disk_width"      , 1.0    , "Disk width (model-dependent)")
+        .item("disk_mass"       , 1.0    , "Total disk mass")
         .item("mach_number"     , 10.0   , "Orbital Mach number of the disk")
         .item("nu"              , 0.1    , "Kinematic viscosity [Omega a^2]")
         .item("num_blocks"      , 1      , "Number of blocks per (per direction)")
@@ -369,7 +372,7 @@ impl Tasks
 // ============================================================================
 trait InitialModel: Hydrodynamics
 {
-    fn primitive_at(&self, xy: (f64, f64)) -> Self::Primitive;
+    fn primitive_at(&self, model: &kind_config::Form, xy: (f64, f64)) -> Self::Primitive;
 }
 
 
@@ -378,21 +381,32 @@ trait InitialModel: Hydrodynamics
 // ============================================================================
 impl InitialModel for Isothermal
 {
-    fn primitive_at(&self, xy: (f64, f64)) -> Self::Primitive
+    fn primitive_at(&self, model: &kind_config::Form, xy: (f64, f64)) -> Self::Primitive
     {
+        use std::f64::consts::PI;
+        use libm::{exp, erf, sqrt};
+
         let (x, y) = xy;
         let r0 = f64::sqrt(x * x + y * y);
         let ph = f64::sqrt(1.0 / (r0 * r0 + 0.01));
         let vp = f64::sqrt(ph);
         let vx = vp * (-y / r0);
         let vy = vp * ( x / r0);
-        return hydro_iso2d::Primitive(1.0, vx, vy);
+
+        let rd: f64 = model.get("disk_radius").into();
+        let dr: f64 = model.get("disk_width").into();
+        let md: f64 = model.get("disk_mass").into();
+
+        let total = PI * dr * dr * (exp(-(rd / dr).powi(2)) + sqrt(PI) * rd / dr * (1.0 + erf(rd / dr)));
+        let sigma = md / total * exp(-((r0 - rd) / dr).powi(2));
+
+        return hydro_iso2d::Primitive(sigma, vx, vy);
     }
 }
 
 impl InitialModel for Euler
 {
-    fn primitive_at(&self, xy: (f64, f64)) -> Self::Primitive
+    fn primitive_at(&self, _model: &kind_config::Form, xy: (f64, f64)) -> Self::Primitive
     {
         let (x, y) = xy;
         let r0 = f64::sqrt(x * x + y * y);
@@ -423,10 +437,10 @@ impl<System: Hydrodynamics + InitialModel> Driver<System> where System::Conserve
     {
         Self{system: system}
     }
-    fn initial_solution(&self, block_index: BlockIndex, mesh: &Mesh) -> BlockSolution<System::Conserved>
+    fn initial_solution(&self, block_index: BlockIndex, mesh: &Mesh, model: &kind_config::Form) -> BlockSolution<System::Conserved>
     {
         let u0 = mesh.cell_centers(block_index)
-            .mapv(|x| self.system.primitive_at(x))
+            .mapv(|x| self.system.primitive_at(model, x))
             .mapv(|p| self.system.to_conserved(p))
             .to_shared();
 
@@ -436,26 +450,26 @@ impl<System: Hydrodynamics + InitialModel> Driver<System> where System::Conserve
             orbital_elements_change: ItemizedChange::zeros(),
         }
     }
-    fn initial_state(&self, mesh: &Mesh) -> State<System::Conserved>
+    fn initial_state(&self, mesh: &Mesh, model: &kind_config::Form) -> State<System::Conserved>
     {
         State{
             time: 0.0,
             iteration: Rational64::new(0, 1),
-            solution: mesh.block_indexes().iter().map(|&i| self.initial_solution(i, mesh)).collect()
+            solution: mesh.block_indexes().iter().map(|&i| self.initial_solution(i, mesh, model)).collect()
         }
     }
     fn initial_time_series(&self) -> Vec<TimeSeriesSample<System::Conserved>>
     {
         Vec::new()
     }
-    fn block_data(&self, mesh: &Mesh) -> Vec<BlockData<System::Conserved>>
+    fn block_data(&self, mesh: &Mesh, model: &kind_config::Form) -> Vec<BlockData<System::Conserved>>
     {
         mesh.block_indexes().iter().map(|&block_index| {
             BlockData{
                 cell_centers:      mesh.cell_centers(block_index).to_shared(),
                 face_centers_x:    mesh.face_centers_x(block_index).to_shared(),
                 face_centers_y:    mesh.face_centers_y(block_index).to_shared(),
-                initial_conserved: self.initial_solution(block_index, &mesh).conserved,
+                initial_conserved: self.initial_solution(block_index, mesh, model).conserved,
                 index: block_index,
             }
         }).collect()
@@ -514,13 +528,12 @@ fn run<S, C>(driver: Driver<S>, app: App, model: kind_config::Form) -> anyhow::R
 {
     let solver     = create_solver(&model, &app);
     let mesh       = create_mesh(&model);
-    let block_data = driver.block_data(&mesh);
+    let block_data = driver.block_data(&mesh, &model);
     let tfinal     = f64::from(model.get("tfinal"));
     let dt         = solver.min_time_step(&mesh);
-    let mut state  = app.restart_file()?.map(io::read_state(&driver.system)).unwrap_or_else(|| Ok(driver.initial_state(&mesh)))?;
+    let mut state  = app.restart_file()?.map(io::read_state(&driver.system)).unwrap_or_else(|| Ok(driver.initial_state(&mesh, &model)))?;
     let mut tasks  = app.restart_file()?.map(io::read_tasks).unwrap_or_else(|| Ok(Tasks::new()))?;
     let mut time_series = app.restart_rundir_child("time_series.h5")?.map(io::read_time_series).unwrap_or_else(|| Ok(driver.initial_time_series()))?;
-
 
     if let Some(last_sample) = time_series.last() {
         if state.time < last_sample.time {
@@ -532,11 +545,11 @@ fn run<S, C>(driver: Driver<S>, app: App, model: kind_config::Form) -> anyhow::R
         }
     }
 
-
     println!();
     for key in &model.sorted_keys() {
         println!("\t{:.<25} {: <8} {}", key, model.get(key), model.about(key));
     }
+
     println!();
     println!("\trestart file            = {}",      app.restart_file()?.map(|f|f.to_string()).unwrap_or("none".to_string()));
     println!("\tcompute units           = {:.04}",  app.compute_units(block_data.len()));
