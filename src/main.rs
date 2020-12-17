eebd454da6afcd30c58cfc5a83ed4e260c968147
Ryan WS <jwestern@uoguelph.ca>
2020-12-17 11:49:46 -0500

    Change default beta from 0.1 to 0.05

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,7 @@ fn main() -> anyhow::Result<()>
         .item("eccentricity"    , 0.0    , "Orbital eccentricity")
         .item("hydro"           , "iso"  , "Hydrodynamics mode: [iso|euler]")
         .item("lambda"          , 0.0    , "Bulk viscosity [Omega a^2] (Farris14:lambda=-nu/3; div3d.v=0:lambda=2nu/3")
-        .item("beta"            , 0.1    , "Beta cooling strength [dimensionless]")
+        .item("beta"            , 0.05   , "Beta cooling strength [dimensionless, ~0.05]")
         .item("mach_number"     , 10.0   , "Orbital Mach number of the disk")
         .item("mass_ratio"      , 1.0    , "Binary mass ratio (M2 / M1)")
         .item("nu"              , 0.001  , "Shear viscosity [Omega a^2]")
