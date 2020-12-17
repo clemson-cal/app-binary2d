c4a93b803d58bf9d5b4c18f3e59313b85b7c16d2
Jack Hu <zhongth@g.clemson.edu>
2020-12-17 18:01:54 -0500

    Fix plot.py merge comments

--- tools/plot.py
+++ tools/plot.py
@@ -6,6 +6,8 @@ import h5py
 import matplotlib.pyplot as plt
 
 
+
+
 def extent(filename):
     """
     Return the domain extent for a checkpoint named `filename` in the format
@@ -13,6 +15,8 @@ def extent(filename):
     """
     r0 = h5py.File(filename, 'r')['model']['domain_radius'][()]
     return [-r0, r0, -r0, r0]
+
+
 
 
 def conserved(filename, field):
@@ -31,12 +35,10 @@ def conserved(filename, field):
     bs = h5f['model']['block_size'][()]
     result = np.zeros([bs * nb, bs * nb])
     for (i, j), v in blocks:
-<<<<<<< HEAD
-        result[i * bs:i * bs + bs, j * bs:j * bs + bs] = v[...][:, :, field]
-=======
         result[i*bs:i*bs+bs, j*bs:j*bs+bs] = v[()][str(field)]
->>>>>>> 1456b0cfb57c95bab6f58b9ae97e35c9ffb77730
     return result
+
+
 
 
 if __name__ == "__main__":
@@ -45,12 +47,6 @@ if __name__ == "__main__":
     parser.add_argument('--range', default='None,None', help='vmin and vmax parameters for the relief plot')
     args = parser.parse_args()
 
-<<<<<<< HEAD
-    rho = conserved(args.filename, 0)
-    plt.imshow(np.log10(rho).T, cmap='inferno', origin='lower',
-               extent=extent(args.filename), vmin=-3, vmax=1.5)
-    plt.colorbar()
-=======
     vmin, vmax = eval(args.range)
 
     for filename in args.filenames:
@@ -59,5 +55,4 @@ if __name__ == "__main__":
         rho = conserved(filename, 0)
         ax1.imshow(np.log10(rho).T, cmap='inferno', origin='lower', extent=extent(filename), vmin=vmin, vmax=vmax)
         # plt.colorbar()
->>>>>>> 1456b0cfb57c95bab6f58b9ae97e35c9ffb77730
     plt.show()
