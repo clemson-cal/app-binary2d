0ebdff614c7cc2f10592d09a7ed2270d28899404
Jack Hu <zhongth@g.clemson.edu>
2021-02-12 08:58:20 -0500

    Remove personal help scripts

--- tools/movie_maker.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-os.system("ffmpeg -f image2 -r 24 -i /Users/jackhu/Google_Drive/Rust_personal"
-          "/projects/app-binary2d/movies/chkpt.%04d.h5.jpg -vcodec mpeg4 "
-          "-y /Users/jackhu/Google_Drive/Rust_personal/projects/app-binary2d"
-          "/movies/movie.mp4")
-
