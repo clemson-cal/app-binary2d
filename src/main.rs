c2dad59933de0a0b54e4c33ce04971858b5b3d7e
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-04-26 21:35:19 -0400

    Lints

--- src/main.rs
+++ src/main.rs
@@ -132,16 +132,16 @@ where
         if crash.is_some() {
             if let Some(safe_cfl) = physics.safe_cfl {
                 physics.cfl = safe_cfl
-            };
+            }
             if let Some(safe_plm) = physics.safe_plm {
                 physics.plm = safe_plm
-            };
+            }
             if let Some(safe_mach_ceiling) = physics.safe_mach_ceiling {
                 physics.mach_ceiling = Some(safe_mach_ceiling)
-            };
+            }
             if let Some(safe_rk_order) = physics.safe_rk_order {
                 physics.rk_order = safe_rk_order
-            };
+            }
         }
         physics
     };
